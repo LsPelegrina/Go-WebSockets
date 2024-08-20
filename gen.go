8cb4c2b9abf817d7b61bb0ec2adfd5bf380f62b7
LsPelegrina <lspelegrina@gmail.com>
2024-08-20 01:18:00 -0300

    Final Commit - Feat: Implement all routes, Implemente utils Fix: Docker compose

--- gen.go
+++ gen.go
@@ -1,4 +1,8 @@
 package gen
 
+//go:generate docker compose up -d
 //go:generate go run ./cmd/tools/terndotenv/main.go
 //go:generate sqlc generate -f ./internal/store/pgstore/sqlc.yml
+//go:generate cyclonedx-gomod mod -licenses -type application -output bom.xml
+//go:generate govulncheck ./...
+//go:generate go run ./cmd/gows/main.go
